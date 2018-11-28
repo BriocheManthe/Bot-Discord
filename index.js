dfc187b4125ffb3e0a05059501451932717b7ec4
BriocheManthe <45418196+BriocheManthe@users.noreply.github.com>
2018-11-28 10:08:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const Discord = require('discord.js');
 
 var bot = new Discord.Client();
 
-bot.login(process.env.TOKEN)
+bot.login("process.env.TOKEN");
 
 bot.on("ready", function() {
 
     bot.user.setGame("Support Nolimit");
     console.log("Le bot est connecté");
 });
+
+client.on('message', message => {
+    if(message.content === "Bonjour")
+    message.reply("Salut")
+});
+
+
+
+
+
+
