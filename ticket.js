d0af41e38d798aba96b89d86d9c128bfd91398af
BriocheManthe <45418196+BriocheManthe@users.noreply.github.com>
2018-11-28 09:48:04 +0100

    Add files via upload

---
+++ ticket.js
@@ -0,0 +1,112 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    // ID van de categorie van de tickets.
+    const categoryId = "517067673432227841";
+
+    // Verkrijg Gebruikersnaam
+    var userName = message.author.username;
+    // Verkrijg discriminator
+    var userDiscriminator = message.author.discriminator;
+
+    // Als ticket al gemaakt is
+    var bool = false;
+
+    // Kijk na als ticket al gemaakt is.
+    message.guild.channels.forEach((channel) => {
+
+        // Als ticket is gemaakt, zend bericht.
+        if (channel.name == userName.toLowerCase() + "-" + userDiscriminator) {
+
+            message.channel.send("Vous avez déja créé un ticket");
+
+            bool = true;
+
+        }
+
+    });
+
+    // Als ticket return code.
+    if (bool == true) return;
+
+    var embedCreateTicket = new discord.RichEmbed()
+        .setTitle("Hoi, " + message.author.username)
+        .setFooter("Vous test");
+
+    message.channel.send(embedCreateTicket);
+
+    // Maak kanaal en zet in juiste categorie.
+    message.guild.createChannel(userName + "-" + userDiscriminator, "text").then((createdChan) => { // Maak kanaal
+
+        createdChan.setParent(categoryId).then((settedParent) => { // Zet kanaal in category.
+
+            // Zet perms voor iedereen
+            settedParent.overwritePermissions(message.guild.roles.find('name', "@everyone"), { "READ_MESSAGES": false });
+            // Zet perms voor de gebruiker die ticket heeft aangemaakt.
+            settedParent.overwritePermissions(message.author, {
+
+                "READ_MESSAGES": true, "SEND_MESSAGES": true,
+                "ATTACH_FILES": true, "CONNECT": true,
+                "CREATE_INSTANT_INVITE": false, "ADD_REACTIONS": true
+
+            });
+
+            var embedParent = new discord.RichEmbed()
+                .setTitle("Hoi, " + message.author.username.toString())
+                .setDescription("Zet hier je vraag/bericht");
+
+            settedParent.send(embedParent);
+
+        }).catch(err => {
+            message.channel.send("Quelque chose s'est mal passé.");
+        });
+
+    }).catch(err => {
+        message.channel.send("Quelque chose s'est mal passé.");
+    });
+
+}
+
+module.exports.help = {
+    name: "ticket"
+}
+
+
+
+
+
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    // Id van category van tickets.
+    const categoryId = "517067673432227841";
+
+    // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
+    if (message.channel.parentID == categoryId) {
+
+        message.channel.delete();
+
+    } else {
+
+        message.channel.send("Gelieve dit commando in een ticket kanaal te doen.");
+
+    }
+
+    var embedCloseTicket = new discord.RichEmbed()
+        .setTitle("Hoi, " + message.channel.name)
+        .setDescription("Je ticket is gemarkeerd als **compleet**. Wil je een nieuwe maken doe dan !ticket")
+        .setFooter("ticket gesloten");
+
+    // Vind kanaal voor de logs.
+    var logChannel = message.guild.channels.find("name", "log");
+    if (!logChannel) return message.channel.send("Kanaal bestaat niet");
+
+    logChannel.send(embedCloseTicket);
+
+}
+
+module.exports.help = {
+    name: "close"
+}
